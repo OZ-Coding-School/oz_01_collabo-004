391f19084c435796daf87b9ee6d74fc0a6812140
taeyuls <startez03@gmail.com>
2024-03-26 18:36:02 +0900

    프론트엔드 초기 세팅

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./Component/Navbar";
+import Home from "./Page/HomePage";
+import MyInfo from "./Page/MyInfoPage";
+import PetHouse from "./Page/PetHousePage";
+import PetRestaurant from "./Page/PetRestaurantPage";
+import PetShop from "./Page/PetShopPage";
+import Service from "./Page/ServicePage";
+import Travel from "./Page/TravelPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/petshop" element={<PetShop />} />
+        <Route path="/pethouse" element={<PetHouse />} />
+        <Route path="/petrestaurant" element={<PetRestaurant />} />
+        <Route path="/travel" element={<Travel />} />
+        <Route path="/myinfo" element={<MyInfo />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="*" element={<h2>Page Not Found</h2>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/Page/PetHousePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PetHousePage = () => {
+  return <div>PetHousePage</div>;
+};
+
+export default PetHousePage;
