1e5d7d97db46e3845f47504bbcd99df42600c921
taeyuls <startez03@gmail.com>
2024-03-27 18:33:29 +0900

    로그인페이지

--- frontend/src/Component/Header/index.jsx
+++ frontend/src/Component/Header/index.jsx
@@ -1,11 +1,11 @@
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './index.css';
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
 
 function Search() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleSearch = () => {
     console.log("검색어:", searchTerm);
@@ -16,29 +16,35 @@ function Search() {
   };
 
   return (
-    <div className='header-container'>
-      <div className='header-title'>
-        <h2> (logo) Dog<span>Go</span></h2>
+    <div className="header-container">
+      <div className="header-title">
+        <h2>
+          {" "}
+          (logo) Dog<span>Go</span>
+        </h2>
       </div>
-      <div className='header-search'>
-        <div className='header-search_input'>
+      <div className="header-search">
+        <div className="header-search_input">
           <input
             type="text"
             placeholder="검색어를 입력하세요"
             value={searchTerm}
             onChange={handleChange}
           />
-          <div className='header-search_icon'
-           onClick={handleSearch}>
+          <div className="header-search_icon" onClick={handleSearch}>
             <FontAwesomeIcon icon={faSearch} />
           </div>
         </div>
       </div>
-      <div className='header-login'>
+      <div className="header-login">
         <ul>
           <span>웰컴</span>
-          <Link><li>로그인</li></Link>
-          <Link><li>고객센터</li></Link>
+          <Link to="/login">
+            <li>로그인</li>
+          </Link>
+          <Link to="/service">
+            <li>고객센터</li>
+          </Link>
         </ul>
       </div>
     </div>
--- frontend/src/Component/Header/navbar/index.jsx
+++ frontend/src/Component/Header/navbar/index.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './index.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
 
 function Navbar() {
   return (
-    <div className='navbar'>
-      <div className='navbar-links'>
+    <div className="navbar">
+      <div className="navbar-links">
         <ul>
-          <li><Link to="/">홈</Link></li>
-          <li><Link to="/petshop">반려 여행 상품</Link></li>
-          <li><Link to="/pethouse">반려 동반 숙소</Link></li>
-          <li><Link to="/petrestaurant">반려 동반 식당</Link></li>
-          <li><Link to="/travel">반려 추천 여행지</Link></li>
+          <li>
+            <Link to="/">홈</Link>
+          </li>
+          <li>
+            <Link to="/petshop">반려 여행 상품</Link>
+          </li>
+          <li>
+            <Link to="/pethouse">반려 동반 숙소</Link>
+          </li>
+          <li>
+            <Link to="/petrestaurant">반려 동반 식당</Link>
+          </li>
+          <li>
+            <Link to="/travel">반려 추천 여행지</Link>
+          </li>
         </ul>
       </div>
     </div>
