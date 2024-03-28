a9d2571087fa448fc2d4f4c1b8b39cdd00c8e3e7
hyeongbin0516 <gudqls0516@gmail.com>
2024-03-28 15:46:02 +0900

    ✨ feat: django-user-model

--- backend/app/users/models.py
+++ backend/app/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin
+)
+# 사용자
+class User(AbstractBaseUser, PermissionsMixin):
+    user_id = models.CharField(unique=True, max_length=15)
+    email = models.EmailField(unique=True, max_length=30)
+    phone = models.CharField(max_length=15)
+    name = models.CharField(max_length=20)
+    ##category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-# Create your models here.
+    USERNAME_FIELD = 'user_id'
