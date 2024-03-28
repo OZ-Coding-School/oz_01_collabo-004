a678ab176c014089e11f2e3f7b269f6ce9760c25
hyeongbin0516 <gudqls0516@gmail.com>
2024-03-28 16:47:28 +0900

    ✨ feat : django-order-model

--- backend/app/orders/models.py
+++ backend/app/orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from common.models import CommonModel
+from products.models import Product
+from users.models import User
 
-# Create your models here.
+# 주문
+class Orders(CommonModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    payment_method = models.CharField(max_length=100)
+    sale_price = models.IntegerField()
+    total_price = models.IntegerField()
+    status = models.BooleanField(default=False)
+    
