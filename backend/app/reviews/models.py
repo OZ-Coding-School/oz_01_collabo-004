de3b9e3c147be9f6aefa5ec2c902b39ec69dae44
Meoyoug <eodyd426@gmail.com>
2024-03-28 16:26:22 +0900

    ✨ feat : django-reviews-model add

--- backend/app/reviews/models.py
+++ backend/app/reviews/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from common.models import CommonModel
+from products.models import Product
+from users.models import User
 
-# Create your models here.
+
+class ProductReview(CommonModel):
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=500)
+    image_url = models.URLField(blank=True, null=True)
+
+    product = models.ForeignKey(Product)
