d61b0a93350ef18771921ce9bbe8d49b1e03d122
Meoyoug <eodyd426@gmail.com>
2024-03-28 14:27:11 +0900

    ✨ feat : product 데이터베이스 모델 추가

--- backend/app/products/models.py
+++ backend/app/products/models.py
@@ -1,3 +1,14 @@
+from common.models import CommonModel
 from django.db import models
 
-# Create your models here.
+
+class Product(CommonModel):
+    name = models.CharField(max_length=100)
+    description_img = models.URLField(null=True, default=None)
+    description_text = models.TextField(null=True, default=None)
+    price = models.IntegerField()
+    sale = models.IntegerField(null=True, default=None)
+    view_count = models.IntegerField(null=False, default=0)
+    status = models.BooleanField(null=False, default=False)
+
+    
