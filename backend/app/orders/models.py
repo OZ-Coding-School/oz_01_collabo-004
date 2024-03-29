5438ebfce67272f387a1fdab2a591ac44b951677
Meoyoug <eodyd426@gmail.com>
2024-03-29 13:24:58 +0900

    💡 chore : Order 모델의fk 수정

--- backend/app/orders/models.py
+++ backend/app/orders/models.py
@@ -4,9 +4,9 @@ from products.models import Product
 from users.models import User
 
 # 주문
-class Orders(CommonModel):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Order(CommonModel):
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     payment_method = models.CharField(max_length=100)
     sale_price = models.IntegerField()
     total_price = models.IntegerField()
