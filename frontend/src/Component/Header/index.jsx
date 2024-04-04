ae19ea7075e69d82d67980c20518606f1f562653
deokgeneral <alsejrrl@naver.com>
2024-04-04 19:21:47 +0900

    ✨ feat: 메인페이지작성

--- frontend/src/Component/Header/index.jsx
+++ frontend/src/Component/Header/index.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 import Navbar from '../navbar';
 import './index.css';
 
-function Headerbar() {
+function Search() {
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 0) {
-        setScrolled(true); 
+        setScrolled(true); // 스크롤이 발생하면 scrolled를 true로 설정
       } else {
-        setScrolled(false);
+        setScrolled(false); // 스크롤이 상단으로 올라가면 scrolled를 false로 설정
       }
     };
 
@@ -33,14 +33,13 @@ function Headerbar() {
       <div className="header-login">
         <ul>
           <Link to="/login"><li>로그인</li></Link>
-          <Link to="/mypage"><li>마이페이지</li></Link>
+          <Link to="/mypage"><li>마이도꼬</li></Link>
         </ul>
       </div>
     </div>
   );
 }
 
-export default Headerbar;
-
+export default Search;
 
 
