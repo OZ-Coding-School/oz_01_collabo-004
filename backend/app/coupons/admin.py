7fb596da50d659f0697a191a699dd123c61ad6aa
Meoyoug <eodyd426@gmail.com>
2024-04-09 20:54:22 +0900

    ✨ feat : Coupon, UserCoupon 모델, 기능, 테스트코드 추가

--- backend/app/coupons/admin.py
+++ backend/app/coupons/admin.py
@@ -1,3 +1,104 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserCoupon, Coupon
+
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    # 어드민 페이지에서 상세페이지로 볼 정보들
+    fieldsets = (
+        (
+            "쿠폰 정보",
+            {
+                "fields": (
+                    "id",
+                    "type",
+                    "content",
+                    "sale_price",
+                    "duration",
+                )
+            },
+        ),
+    )
+
+    # 쿠폰을 만들때 보이는 화면
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "type",
+                    "content",
+                    "sale_price",
+                    "duration",
+                ),
+            },
+        ),
+    )
+
+    # 표에서 보이는 정보
+    list_display = (
+        "id",
+        "type",
+        "sale_price",
+        "duration",
+    )
+    search_fields = ("type", "content", "sale_price")
+    ordering = ("id",)
+
+
+@admin.register(UserCoupon)
+class UserCouponAdmin(admin.ModelAdmin):
+    def coupon_type(self, obj):
+        return obj.coupon.type
+
+    def coupon_sale_price(self, obj):
+        return obj.coupon.sale_price
+
+    def coupon_duration(self, obj):
+        return obj.coupon.duration
+
+    # 어드민 페이지에서 상세페이지로 볼 정보들
+    fieldsets = (
+        (
+            "쿠폰 정보",
+            {
+                "fields": (
+                    "user",
+                    "coupon",
+                    "expired_at"
+                )
+            },
+        ),
+    )
+
+    # 유저쿠폰을 만들때 보이는 화면
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "coupon_id",
+                    "user_id",
+                    "expired_at",
+                ),
+            },
+        ),
+    )
+
+    # 표에서 보이는 정보
+    list_display = (
+        "id",
+        "coupon_id",
+        "user_id",
+        "coupon_type",
+        "coupon_sale_price",
+        "coupon_duration",
+        "expired_at",
+    )
+    search_fields = ("user_id", "coupon_id", "expired_at")
+    ordering = ("id",)
+
+
--- backend/app/coupons/serializers.py
+++ backend/app/coupons/serializers.py
@@ -1,3 +1,26 @@
 from rest_framework import serializers
 
 from .models import Coupon, UserCoupon
+
+
+class CouponSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coupon
+        fields = "__all__"
+
+
+class CouponInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coupon
+        exclude = ["created_at", "modified_at", "id"]
+
+
+class UserCouponSerializer(serializers.ModelSerializer):
+    coupon_info = CouponInfoSerializer(source="coupon", read_only=True)
+
+    class Meta:
+        model = UserCoupon
+        fields = "__all__"
+        read_only_fields = [
+            "status",
+        ]
