2f8d47b3b9b497a77971f05f54cf1068919d2920
Meoyoug <eodyd426@gmail.com>
2024-04-15 23:56:52 +0900

    🔧 fix : travel_period 필드 추가

--- app/products/models.py
+++ app/products/models.py
@@ -5,10 +5,11 @@ from common.models import CommonModel
 
 class Product(CommonModel):
     name = models.CharField(max_length=100)
-    product_img = models.URLField(null=True, default=None)
-    description_img = models.URLField(null=True, default=None)
-    description_text = models.TextField(null=True, default=None)
-    price = models.IntegerField()
-    sale = models.IntegerField(null=True, default=0)
+    product_img = models.ImageField(upload_to="product/img/thumbnail", default=None)
+    description_img = models.ImageField(upload_to="product/img/description", default=None)
+    description_text = models.TextField(default=None)
+    price = models.IntegerField(default=0)
+    travel_period = models.IntegerField(default=0)
+    sale = models.IntegerField(default=0)
     view_count = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
