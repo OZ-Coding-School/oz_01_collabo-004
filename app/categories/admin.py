a62423b96f33f0791197286248c01072a9429e66
hyeongbin0516 <gudqls0516@gmail.com>
2024-04-16 17:35:04 +0900

    ✨ feat : category admin 추가

--- app/categories/admin.py
+++ app/categories/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, CategoryProductConnector, CategoryUserConnector
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_filter = ("name",)
+    list_display_links = ("name",)
+    search_fields = ("name",)
+    ordering = ("id",)
+
+    class CategoryProductConnectorInline(admin.StackedInline):
+        model = CategoryProductConnector
+
+    inlines = [CategoryProductConnectorInline]
+
+
+@admin.register(CategoryUserConnector)
+class CategoryUserConnectorAdmin(admin.ModelAdmin):
+    list_display = ("user", "category")
+    list_filter = ("category", "user")
+    list_display_links = ("user",)
+    search_fields = ("user", "category")
+    ordering = ("id",)
+
+
+@admin.register(CategoryProductConnector)
+class CategoryProductConnectorAdmin(admin.ModelAdmin):
+    list_display = ("product", "category")
+    list_filter = ("category", "product")
+    list_display_links = ("product",)
+    search_fields = ("product", "category")
+    ordering = ("id",)
