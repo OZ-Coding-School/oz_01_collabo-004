3ed8bec2792c1c3c9390fcf68c4eda2cf458a7bd
Meoyoug <eodyd426@gmail.com>
2024-04-18 15:48:21 +0900

    ✨ feat : 상품에 대한 어드민 작성, 쿠폰 모델의 메서드 수정
    - 장고 어드민 오버라이드해서 객체 삭제하는 부분 수정
    - 상품을 삭제, 상태변경, 조회수 초기화 할 수 있는 액션 추가
    - 상품의 정보를 리스트로 보여줄 수 있음
    - Product 모델의 이미지필드를 활용, s3에 업로드하기 위한 url 생성하는 함수 추가

--- app/products/admin.py
+++ app/products/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from common.utils import S3ImgUploader
+
 from .models import Product
 
 
@@ -7,44 +9,75 @@ from .models import Product
 class ProductAdmin(admin.ModelAdmin):
     # 어드민 페이지에서 상세페이지로 볼 상품 정보들
     fieldsets = (
-        (
-            "상품 정보",
-            {
-                "fields": (
-                    "name",
-                    # 'product_img',
-                    # 'description_img',
-                    # 'description_text',
-                    "price",
-                    "sale",
-                    "status",
-                    "view_count",
-                )
-            },
-        ),
-    )
-
-    # 상품을 만들때 보이는 화면
-    add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": (
-                    "name",
-                    # 'product_img',
-                    # 'description_img',
-                    # 'description_text',
-                    "price",
-                    "sale",
-                    "status",
-                    "view_count",
-                ),
-            },
-        ),
+        ("Product Info", {"fields": ("name", "product_img", "description_img", "description_text", "travel_period")}),
+        ("Price and Sale", {"fields": ("price", "discount")}),
+        ("Status and View Count", {"fields": ("status", "view_count")}),
     )
 
     # 표에서 보이는 정보
-    list_display = ("name", "description_text", "price", "sale", "status", "view_count")
+    list_display = ("name", "description_text", "price", "discount", "status", "view_count")
     search_fields = ("price", "name", "description_text")
     ordering = ("id",)
+
+    # 기본 actions에 세팅된 delete_selected 제거
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if "delete_selected" in actions:
+            del actions["delete_selected"]
+        return actions
+
+    def save_model(self, request, obj, form, change):
+        obj.save()
+
+    def delete_model(self, request, obj):
+        image_uploader = S3ImgUploader()
+        if obj.product_img:
+            image_uploader.delete_img_file(str(obj.product_img))
+        if obj.description_img:
+            image_uploader.delete_img_file(str(obj.description_img))
+
+        super().delete_model(request, obj)
+
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        """
+        ModelAdmin.change_view를 오버라이드해서 업데이트 시 기존의 이미지는 삭제하고 새로운 이미지를 S3에 저장
+        """
+        update_product = self.get_object(request, object_id)
+        if update_product is not None:
+            if update_product.product_img is not None:
+                image_uploader = S3ImgUploader()
+                image_uploader.delete_img_file(str(update_product.product_img))
+            if update_product.description_img is not None:
+                image_uploader = S3ImgUploader()
+                image_uploader.delete_img_file(str(update_product.product_img))
+            return super().change_view(request, object_id, form_url=form_url, extra_context=extra_context)
+        return None
+
+    def delete_selected_model(self, request, queryset):
+        for obj in queryset:
+            self.delete_model(request, obj)
+
+    def start_sales_products(self, request, queryset):
+        if queryset.count() > 0:
+            for obj in queryset:
+                obj.status = True
+                obj.save()
+
+    def stop_sales_products(self, request, queryset):
+        if queryset.count() > 0:
+            for obj in queryset:
+                obj.status = False
+                obj.save()
+
+    def reset_view_count(self, request, queryset):
+        if queryset.count() > 0:
+            for obj in queryset:
+                obj.view_count = 0
+                obj.save()
+
+    # actions 수정
+    delete_selected_model.short_description = "Delete selected products"
+    start_sales_products.short_description = "Start sales products"
+    stop_sales_products.short_description = "Stop sales products"
+    reset_view_count.short_description = "Reset view count"
+    actions = ["delete_selected_model", "start_sales_products", "stop_sales_products", "reset_view_count"]
--- app/products/models.py
+++ app/products/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 
 from common.models import CommonModel
+from common.utils import get_random_id
+
+
+def upload_to_thumbnail(instance: models.Model, filename: str) -> str:
+    # 파일명은 랜덤한 8자리의 문자열과 업로드한 파일이름을 조합해서 만듦(유일성 보장)
+    return f"products/thumbnails/{get_random_id()} + {filename}"
+
+
+def upload_to_description(instance: models.Model, filename: str) -> str:
+    # 파일명은 랜덤한 8자리의 문자열과 업로드한 파일이름을 조합해서 만듦(유일성 보장)
+    return f"products/descriptions/{get_random_id()} + {filename}"
 
 
 class Product(CommonModel):
     name = models.CharField(max_length=100)
-    product_img = models.ImageField(null=True, upload_to="product/img/thumbnail", default=None)
-    description_img = models.ImageField(null=True, upload_to="product/img/description", default=None)
+    product_img = models.ImageField(null=True, upload_to=upload_to_thumbnail, default=None)
+    description_img = models.ImageField(null=True, upload_to=upload_to_description, default=None)
     description_text = models.TextField(default=None)
     price = models.IntegerField(default=0)
     travel_period = models.IntegerField(default=0)
-    sale = models.IntegerField(default=0)
+    discount = models.IntegerField(default=0)
     view_count = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
 
