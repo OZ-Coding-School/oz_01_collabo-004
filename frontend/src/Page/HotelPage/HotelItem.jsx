68502b99e1aefaa66cb2273cce693b95c773b1cb
taeyuls <startez03@gmail.com>
2024-04-18 15:14:38 +0900

    ✨ feat :호텔 페이지 생성

---
+++ frontend/src/Page/HotelPage/HotelItem.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const HotelItem = ({ spot }) => {
+  return (
+    <div
+      style={{
+        margin: "10px",
+        textAlign: "center",
+      }}
+      className="hotel-item"
+    >
+      <div style={{ margin: "10px" }}>
+        <h3>{spot.name}</h3>
+        <p>{spot.description}</p>
+        <a target="_blank" style={{ textDecoration: "none" }} href={spot.link}>
+          🏠Click Me!
+        </a>
+        <hr />
+        <img
+          style={{
+            width: "700px",
+            height: "350px",
+          }}
+          src={spot.image}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HotelItem;
