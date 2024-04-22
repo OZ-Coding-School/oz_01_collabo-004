8f3fce8a159ddae877de1842d7801b3b8dbcee77
Meoyoug <eodyd426@gmail.com>
2024-04-22 12:01:24 +0900

    🔧 fix : request.FILES에서 이미지 파일을 가져와서 시리얼라이저를 통해 저장하도록 변경.
    - 리뷰 수정시 이미지 파일의 변경요청이 있으면 기존의 이미지는 s3에서 삭제하고 새로운 이미지를 저장하는 로직 추가

--- app/reviews/views.py
+++ app/reviews/views.py
@@ -38,12 +38,11 @@ class MyReviewListView(APIView):
         description="Get products reviews, After Post product-review",
     )
     def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        data = {
-            "product": request.data.get("product"),
-            "title": request.data.get("title"),
-            "content": request.data.get("content"),
-        }
-        serializer = CreateReviewSerializer(data=data)
+        data = request.data
+        image_file = request.FILES.get("image_file")
+        if image_file:
+            data["image_url"] = image_file
+        serializer = CreateReviewSerializer(data=data, partial=True)
         if serializer.is_valid():
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -59,14 +58,22 @@ class ProductReviewDetailView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request: Request, review_id: int, *args: Any) -> Response:
-        review = get_object_or_404(ProductReview, id=review_id)
-        if review.user == request.user:  # 리뷰를 작성한 유저가 보낸 수정 요청인지를 확인함
-            serializer = ProductReviewDetailSerializer(review, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
+        review = get_object_or_404(ProductReview, id=review_id, user=request.user.id)
+        update_data = request.data
+        update_image = request.FILES.get("image_file")
+        if update_image:
+            update_data["image_url"] = update_image
+        serializer = ProductReviewDetailSerializer(review, data=request.data, partial=True)
+        if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+        if update_image:
+            try:
+                image_uploader = S3ImgUploader()
+                image_uploader.delete_img_file(str(review.image_url))  # 기존의 리뷰 이미지를 삭제
+            except Exception as e:
+                return Response({"msg": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        serializer.save()  # 인스턴스가 저장되면서 s3에 새로운 이미지가 업로드됨.
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request: Request, review_id: int, *args: Any) -> Response:
         review = get_object_or_404(ProductReview, id=review_id)
@@ -77,32 +84,32 @@ class ProductReviewDetailView(APIView):
         return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
-class ProductReviewImageUploadView(APIView):
-    def post(self, request: Request) -> Response:
-        """
-        이미지 업로드를 시도하면 boto3를 이용해서 s3로 이미지를 업로드하는 메서드
-        """
-        try:
-            image_file = request.FILES["image"]
-            prefix = f"users/reviews/"
-            image_uploader = S3ImgUploader()
-            image_url = image_uploader.upload(image_file, prefix)
-            return Response({"image_url": image_url}, status=status.HTTP_200_OK)
-        except ValueError as ve:
-            return Response({"error": str(ve)}, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def delete(self, request: Request) -> Response:
-        try:
-            image_file_url = request.data["image_file_url"]
-            image_uploader = S3ImgUploader()
-            response = image_uploader.delete_img_file(image_file_url)
-            if response["status"] in [200, 204]:
-                return Response(response["msg"], status=status.HTTP_200_OK)
-            return Response(response["msg"], status=response["status"])
-        except Exception as e:
-            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+# class ProductReviewImageUploadView(APIView):
+#     def post(self, request: Request) -> Response:
+#         """
+#         이미지 업로드를 시도하면 boto3를 이용해서 s3로 이미지를 업로드하는 메서드
+#         """
+#         try:
+#             image_file = request.FILES["image"]
+#             prefix = f"users/reviews/"
+#             image_uploader = S3ImgUploader()
+#             image_url = image_uploader.upload(image_file, prefix)
+#             return Response({"image_url": image_url}, status=status.HTTP_200_OK)
+#         except ValueError as ve:
+#             return Response({"error": str(ve)}, status=status.HTTP_400_BAD_REQUEST)
+#         except Exception as e:
+#             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+#
+#     def delete(self, request: Request) -> Response:
+#         try:
+#             image_file_url = request.data["image_file_url"]
+#             image_uploader = S3ImgUploader()
+#             response = image_uploader.delete_img_file(image_file_url)
+#             if response["status"] in [200, 204]:
+#                 return Response(response["msg"], status=status.HTTP_200_OK)
+#             return Response(response["msg"], status=response["status"])
+#         except Exception as e:
+#             return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class ProductReviewListView(APIView):
