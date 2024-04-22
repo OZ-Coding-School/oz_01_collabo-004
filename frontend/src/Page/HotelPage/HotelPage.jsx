edb2000f7167086e2d837a25892b44257605289b
taeyuls <startez03@gmail.com>
2024-04-22 14:07:29 +0900

    🔧 fix : 회원가입 수정

--- frontend/src/Page/HotelPage/HotelPage.jsx
+++ frontend/src/Page/HotelPage/HotelPage.jsx
@@ -163,8 +163,7 @@ const HotelPage = () => {
 
   return (
     <div className="hotel-page">
-      <h2 style={{ textAlign: "center", margin: "30px" }}>
-        Hotel With Pet</h2>
+      <h2 style={{ textAlign: "center", margin: "30px" }}>Hotel With Pet</h2>
       <div className="hotel-box">
         {hotelSpots.map((spot) => (
           <HotelItem key={spot.id} spot={spot} />
