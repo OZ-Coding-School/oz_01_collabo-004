c62e98eb2cebd4462ff6bd2b8a4825c9003f876c
Meoyoug <eodyd426@gmail.com>
2024-04-23 18:28:49 +0900

    ✨ feat : 비밀번호 찾기 시 이메일 인증에 필요한 시리얼라이저 추가, 이메일 인증 코드 확인 로직 변경
    - VerificationCodeSerializer에서 입력받은 정보를 바탕으로 검증하는데 타입에 따라서 회원가입과 비밀번호 찾기에 필요한 데이터를 받도록 설정
    
    - 기존의 view에서 하던 검증을 EmailSerializer에서 처리하도록 변경
    - ForgotPasswordSerializer: 이메일과 유저의 아이디를 받아서 인증정보를 검증

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -58,11 +58,26 @@ class UserInfoModifySerializer(serializers.ModelSerializer):
 class VerificationCodeSerializer(serializers.Serializer):
     email = serializers.EmailField()
     code = serializers.CharField(max_length=6)
+    verification_type = serializers.CharField(max_length=15)
+    user_id = serializers.CharField(max_length=15, required=False)
 
     def validate(self, data):
-        email = data.get("email")
-        code = data.get("code")
-        verification_code = cache.get(f"{email}-verify_code")
+        email = data["email"]
+        code = data["code"]
+        verification_type = data["verification_type"]
+
+        # 캐시에서 검색하는 키를 설정
+        if verification_type == "signup":
+            cache_key = f"{email}-verify_code"
+        elif verification_type == "forgot_password" and "user_id" in data:
+            user_id = data["user_id"]
+            cache_key = f"{user_id}-{email}-verify_code"
+        else:
+            raise serializers.ValidationError("Invalid verification type")
+
+        # 캐시에서 검색한 코드 가져오기
+        verification_code = cache.get(cache_key)
+
         if verification_code is None:
             raise serializers.ValidationError("Email Verification time has expired.")
         if code != verification_code:  # 인증코드 일치 여부 확인
@@ -73,3 +88,27 @@ class VerificationCodeSerializer(serializers.Serializer):
 
 class EmailSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True, max_length=30)
+
+    def validate(self, data):
+        email = data.get("email", "")
+        if not email:
+            raise serializers.ValidationError("이메일을 입력해주세요.")
+        elif User.objects.filter(email=email).exists():
+            raise serializers.ValidationError("이미 가입된 이메일입니다.")
+        return data
+
+class ForgotPasswordSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=30)
+    user_id = serializers.CharField(max_length=15)
+
+    def validate(self, data):
+        email = data.get("email")
+        if not email:
+            raise serializers.ValidationError("이메일을 입력해주세요.")
+        user_id = data.get("user_id")
+        if not user_id:
+            raise serializers.ValidationError("아이디를 입력해주세요.")
+        user = User.objects.filter(email=email, user_id=user_id).first()
+        if not user:
+            raise serializers.ValidationError("입력한 아이디와 이메일 인증정보가 일치하지 않습니다.")
+        return data
