4d720704c2b312205d85ea968244fc93ca22ea18
Meoyoug <eodyd426@gmail.com>
2024-04-24 19:00:48 +0900

    🔧 fix : 어드민 url 환경변수에서 가져오도록 변경

--- app/config/urls.py
+++ app/config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 
 from django.contrib import admin
 from django.urls import include, path
@@ -24,7 +25,7 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path(f"{os.environ.get("ADMIN_PAGE_URL")}", admin.site.urls),
     path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/v1/swagger-ui/",
